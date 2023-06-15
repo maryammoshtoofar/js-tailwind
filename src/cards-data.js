e8ce67e5c17c998c17b3181e87fd13e821422e41
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2023-06-15 13:20:23 +0330

    add cards-data.js

---
+++ src/cards-data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    name: "mountains",
+    img: "./images/mountains.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+  {
+    name: "lights",
+    img: "./images/lights.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+  {
+    name: "nature",
+    img: "./images/nature.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+  {
+    name: "p1",
+    img: "./images/p1.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+  {
+    name: "p2",
+    img: "./images/p2.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+  {
+    name: "p3",
+    img: "./images/p3.jpg",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, inventore. Quae, quasinostrum laudantium sapiente consequatur natus et neque tenetur!",
+  },
+];
