bce5516c789a741761a7bd320226b3b65d053e83
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2023-06-15 13:29:28 +0330

    add cards by js DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const nav = document.querySelector(".nav");
 const body = document.querySelector("body");
-
-// function init() {
-//   nav.style.display = "none";
-// }
+import { data } from "./cards-data.js";
 
 function toggleNav() {
   if (nav.classList.contains("hidden")) {
@@ -15,4 +12,23 @@ function toggleNav() {
   }
 }
 
-// document.addEventListener("DOMContentLoaded", init);
+
+function generateCards() {
+  const cardsContainer = document.querySelector(".cards");
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card m-5";
+    const img = document.createElement("img");
+    img.className = "rounded hover:opacity-60 w-full";
+    img.src = item.img;
+    img.alt = item.name;
+    card.append(img);
+    const title = document.createElement("h5");
+    title.class = "capitalize font-semibold my-4";
+    title.innerHTML = item.text;
+    card.append(title);
+    cardsContainer.append(card);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", generateCards);
